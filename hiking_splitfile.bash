1c94a3f4260f1c97cb42c2d61a9503d70ed11ff3
K.O <hikingko1@gmail.com>
2012-11-06 21:39:02 +0900

    splitfile

---
+++ hiking_splitfile.bash
@@ -0,0 +1,36 @@
+Document='
+============================
+hiking_splitfile.bash
+----------------------------
+arguments
+    |- $filename
+(   |- splitnum = 2   )
+output
+============================
+'
+
+# --- arguments ---
+NEEDARGNUM=`echo "$Document"|grep -A 100 "arguments"|grep -B 100 "output"|perl -pe 's/^\(.*\n$//'|wc -l`
+NEEDARGNUM=$(($NEEDARGNUM-2))
+if [ $# -lt $NEEDARGNUM ];then
+   echo "$Document"
+   exit 1
+fi
+filename=$1
+base=${filename%.*}
+base=${base}_split
+ext=.${filename##*.}
+splitnum=2;if [ $# -gt 2 ];then splitnum=$2;fi
+
+# --- main --- #
+linenum=`less $filename|wc -l`
+outlinenum=$(($linenum/$splitnum))
+
+splitnum=$(($splitnum-1))
+less $filename|tail -n +$(($outlinenum*$splitnum)) > ${base}$((splitnum+1))$ext
+echo "less $filename|tail -n +$(($outlinenum*$splitnum))"
+while [ $splitnum -gt 0 ];do
+    splitnum=$(($splitnum-1))
+    less $filename|tail -n +$(($outlinenum*$splitnum))|head -$outlinenum > ${base}$(($splitnum+1))$ext
+    echo "less $filename|tail -n +$(($outlinenum*$splitnum))|head -$outlinenum"
+done
