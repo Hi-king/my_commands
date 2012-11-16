a94b45f5bd451ca97a0fb5d346d8c9fe7f6595c2
K.O <hikingko1@gmail.com>
2012-11-17 04:21:09 +0900

    randomize line

---
+++ hiking_randomizefileline.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+Document='
+============================
+Randomize lines (Speedup)
+----------------------------
+arguments
+    |- $filename
+output
+    |- randomized lines
+============================
+'
+
+# --- arguments ---
+NEEDARGNUM=`echo "$Document"|grep -A 100 "arguments"|grep -B 100 "output"|perl -pe 's/^\(.*\n$//'|wc -l`
+NEEDARGNUM=$(($NEEDARGNUM-2))
+if [ $# -lt $NEEDARGNUM ];then
+   echo "$Document"
+   exit 1
+fi
+
+filename=$1
+
+a=($(less $filename))
+for i in $(seq 1 $((${#a[@]}-1)));do 
+    randi=$(($RANDOM%$i));
+    tmp=${a[$i]};
+    a[$i]=${a[$randi]};
+    a[$randi]=$tmp;
+done;
+
+for i in ${a[*]};do echo $i;done
